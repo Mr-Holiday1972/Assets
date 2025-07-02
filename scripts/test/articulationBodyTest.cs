ab940abf841e6540095aad6e6646e86e12b71e88
Mr-Holiday1972 <hinoki1124@gmail.com>
2025-07-02 14:48:00 +0900

    the first model of the articulation body method (success)

--- scripts/test/articulationBodyTest.cs
+++ scripts/test/articulationBodyTest.cs
@@ -9,26 +9,33 @@ public class articulationBodyTest : MonoBehaviour
     // 定期的に呼ばれる
     void FixedUpdate()
     {
+        //controls the wheel-"arm"'s rotation
+        //forward
         if (Input.GetKey(KeyCode.Q))
         {
-            var xDrive = this.articulationBodies[2].xDrive;
-            xDrive.target -= 10f;
-            this.articulationBodies[2].xDrive = xDrive;
+            var xDrive = this.articulationBodies[2].xDrive;//gets the current articulation bosy's target value
+            xDrive.target -= 10f;//changes the target value to however
+            this.articulationBodies[2].xDrive = xDrive;//applies the changed value
+
+            //the same logic applies to the rest of the arm-controling lines
         }
+        //backward
         else if (Input.GetKey(KeyCode.E))
         {
             var xDrive = this.articulationBodies[2].xDrive;
             xDrive.target += 10f;
             this.articulationBodies[2].xDrive = xDrive;
         }
-
         
-        else if (Input.GetKey(KeyCode.A))
+        //controls the steering-arm's y rotation
+        //forward
+        if (Input.GetKey(KeyCode.A))
         {
             var yValue = this.articulationBodies[1].yDrive;
             yValue.target -= 1f;
             this.articulationBodies[1].yDrive = yValue;
         }
+        //backward
         else if (Input.GetKey(KeyCode.D))
         {
             var yValue = this.articulationBodies[1].yDrive;
@@ -36,6 +43,7 @@ public class articulationBodyTest : MonoBehaviour
             this.articulationBodies[1].yDrive = yValue;
         }
 
+        //gives a gas to and rotates the actual wheel
         if (Input.GetKey(KeyCode.W))
         {
             wheel.AddTorque(0, 0, 10f);
